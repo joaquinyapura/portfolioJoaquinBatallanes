9d5a221cd1e2b61a58818b9e9213d7c31520ca7f
Joaquin <joaquinyapura@gmail.com>
2022-03-21 23:15:12 -0300

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/header/Header'
+import About from "./components/about/About"
+import Contact from './components/contact/Contact'
+import Experience from './components/experience/Experience'
+import References from './components/references/References'
+
+function App() {
+  return (
+
+<>
+
+</>
+
+  )
+}
+
+export default App
---
+++ src/components/references/References.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./references.css"
+
+function References() {
+  return (
+    <div>References</div>
+  )
+}
+
+export default References
